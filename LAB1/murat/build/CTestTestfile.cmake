07dcd23fbfe6c5e81b65f9dd39e1dfff44201dc0
Dinmukhamet <63992649+mdinmukhamet9@users.noreply.github.com>
2024-09-23 00:43:21 +0500

    Delete LAB1 directory

--- LAB1/murat/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dinm/ros1_ws/src/murat
-# Build directory: /home/dinm/ros1_ws/src/murat/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
--- LAB1/murat/build/gtest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest
-# Build directory: /home/dinm/ros1_ws/src/murat/build/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
